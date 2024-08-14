3454fad842529b692d9b9d2da434b0238e4e3b2a
aenewsss <aenamartinelli@gmail.com>
2024-08-14 03:11:16 -0300

    function to get searches included

--- handlers/search.go
+++ handlers/search.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 
 	g "github.com/serpapi/google-search-results-golang"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	config "google-monitoring/config"
 
@@ -33,63 +35,89 @@ type SearchResult struct {
 
 func SearchHandler(client *mongo.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req SearchRequest
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, "Invalid request payload", http.StatusBadRequest)
-			return
-		}
+		if r.Method == "GET" {
+			collection := client.Database(config.LoadConfig().DbName).Collection("searches")
 
-		parameter := map[string]string{
-			"engine":        "google",
-			"location":      req.City,
-			"q":             req.Query,
-			"google_domain": "google.com.br",
-			"gl":            "br",
-			"hl":            "pt-br",
-			"device":        req.Device,
-		}
+			filter := bson.M{}
+			opts := options.Find()
 
-		apiKey := config.LoadConfig().SerpAPIKey
-
-		search := g.NewGoogleSearch(parameter, apiKey)
-
-		results, err := search.GetJSON()
-		if err != nil {
-			http.Error(w, "Failed to get search results", http.StatusInternalServerError)
-			return
-		}
-
-		var adsOrOrganic interface{}
-		var ok bool
-		
-		adsOrOrganic, ok = results["ads"]
-
-		if !ok {
-			adsOrOrganic, ok = results["organic_results"]
-			if !ok {
-				http.Error(w, "'ads' or 'organic_results' field not found in search results", http.StatusNotFound)
+			cursor, err := collection.Find(context.TODO(), filter, opts)
+			if err != nil {
+				http.Error(w, "Failed to retrieve search results", http.StatusInternalServerError)
 				return
 			}
-		}
+			defer cursor.Close(context.TODO())
 
-		adsOrOrganicJSON, err := json.Marshal(adsOrOrganic)
-		if err != nil {
-			http.Error(w, "Failed to convert 'ads' or 'organic_results' to JSON", http.StatusInternalServerError)
+			var searchResults []SearchResult
+			if err := cursor.All(context.TODO(), &searchResults); err != nil {
+				http.Error(w, "Failed to decode search results", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(searchResults)
 			return
 		}
 
-		response, err := GoogleApiSearch(client, adsOrOrganicJSON, req.Query)
-		if err != nil {
-			http.Error(w, "Failed to get google api response", http.StatusInternalServerError)
-			return
-		}
+		if r.Method == "POST" {
+			var req SearchRequest
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				http.Error(w, "Invalid request payload", http.StatusBadRequest)
+				return
+			}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response)
+			parameter := map[string]string{
+				"engine":        "google",
+				"location":      req.City,
+				"q":             req.Query,
+				"google_domain": "google.com.br",
+				"gl":            "br",
+				"hl":            "pt-br",
+				"device":        req.Device,
+			}
+
+			apiKey := config.LoadConfig().SerpAPIKey
+
+			search := g.NewGoogleSearch(parameter, apiKey)
+
+			results, err := search.GetJSON()
+			if err != nil {
+				http.Error(w, "Failed to get search results", http.StatusInternalServerError)
+				return
+			}
+
+			var adsOrOrganic interface{}
+			var ok bool
+
+			adsOrOrganic, ok = results["ads"]
+
+			if !ok {
+				adsOrOrganic, ok = results["organic_results"]
+				if !ok {
+					http.Error(w, "'ads' or 'organic_results' field not found in search results", http.StatusNotFound)
+					return
+				}
+			}
+
+			adsOrOrganicJSON, err := json.Marshal(adsOrOrganic)
+			if err != nil {
+				http.Error(w, "Failed to convert 'ads' or 'organic_results' to JSON", http.StatusInternalServerError)
+				return
+			}
+
+			response, err := GoogleApiSearch(client, adsOrOrganicJSON, req.Query)
+			if err != nil {
+				http.Error(w, "Failed to get google api response", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(response)
+		}
 	}
 }
 
-func GoogleApiSearch(mongoClient *mongo.Client,adsOrOrganicJSON []byte, query string) ([]byte, error) {
+func GoogleApiSearch(mongoClient *mongo.Client, adsOrOrganicJSON []byte, query string) ([]byte, error) {
 	apiKey := config.LoadConfig().CustomSearchAPIKey
 	cx := config.LoadConfig().SearchEngineID
 
